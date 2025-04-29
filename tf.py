807fcbf58d9f23d062d0e3b496d7914b7807ac3f
benoit-lefebvre-caelus <benoit.lefebvre@getcaelus.cloud>
2025-04-29 15:52:38 +0200

    Create tf.py

---
+++ tf.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
+print("Is GPU available:", tf.config.list_physical_devices('GPU'))
+
+#Use python3 tf.py in order to test if tensorflow can use your Graphic Card :)
